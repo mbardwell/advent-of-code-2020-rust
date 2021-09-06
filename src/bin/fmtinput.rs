f409e73d7e1bf20f88677e015c9ae0fc8b9aa2b3
mdbardwell <bardwell@ualberta.ca>
2021-09-05 23:32:53 -0400

    Improve input formatter

--- src/bin/fmtinput.rs
+++ src/bin/fmtinput.rs
@@ -1,9 +1,22 @@
 use std::env;
 use std::fs;
+use regex::Regex;
 
+fn day1(content: &String) {
+    let re = Regex::new(r"(?P<ints>\d+)\n").unwrap();
+    for cap in re.captures_iter(content) {
+        println!("{},", &cap["ints"]);
+    }
+}
 
 fn day2(content: &String) {
-    println!("{:?}", content);
+    let re = Regex::new(
+        r"(?P<cmin>\d+)-(?P<cmax>\d+) (?P<c>\w): (?P<input>\w+)\n"
+    ).unwrap();
+    for cap in re.captures_iter(content) {
+        println!("({},{},b'{}',b\"{}\"),",
+        &cap["cmin"], &cap["cmax"], &cap["c"], &cap["input"]);
+    }
 }
 
 fn main() {
@@ -14,7 +27,7 @@ fn main() {
     let content = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
     match &day {
-        1 => {},
+        1 => day1(&content),
         2 => day2(&content),
         _ => {},
     }
