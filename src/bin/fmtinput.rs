44a7044d2682241054bbb64236552a36a3e83a2e
mdbardwell <bardwell@ualberta.ca>
2021-09-06 10:54:56 -0400

    Day 3

--- src/bin/fmtinput.rs
+++ src/bin/fmtinput.rs
@@ -2,20 +2,26 @@ use std::env;
 use std::fs;
 use regex::Regex;
 
-fn day1(content: &String) {
+fn day1(content: &str) {
     let re = Regex::new(r"(?P<ints>\d+)\n").unwrap();
     for cap in re.captures_iter(content) {
         println!("{},", &cap["ints"]);
     }
 }
 
-fn day2(content: &String) {
+fn day2(content: &str) {
     let re = Regex::new(
         r"(?P<cmin>\d+)-(?P<cmax>\d+) (?P<c>\w): (?P<input>\w+)\n"
     ).unwrap();
     for cap in re.captures_iter(content) {
         println!("({},{},b'{}',b\"{}\"),",
         &cap["cmin"], &cap["cmax"], &cap["c"], &cap["input"]);
+    }
+}
+
+fn day3(content: &str) {
+    for line in content.lines() {
+        println!("b{:?},", line);
     }
 }
 
@@ -29,6 +35,7 @@ fn main() {
     match &day {
         1 => day1(&content),
         2 => day2(&content),
-        _ => {},
+        3 => day3(&content),
+        _ => {}
     }
 }
